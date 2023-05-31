aa4f2ae7bab9722c6af2b82ea7e0bae221e0330d
18kotov <twozerocool@gmail.com>
2023-05-31 10:45:58 +0300

    рабочая версия

---
+++ bot/handlers/basic.py
@@ -0,0 +1,56 @@
+import os
+import time
+import datetime
+from aiogram.types import Message
+from aiogram import Router
+from aiogram import F
+from aiogram.filters import CommandStart, Command
+from logic.prepare_info import get_list_messages_for_today, get_quantity_phrases_repeat_today
+from settings import get_logger
+
+
+logger = get_logger(__name__)
+
+user1 = int(os.getenv('user1'))
+user2 = int(os.getenv('user2'))
+users = {user1, user2}
+
+router = Router()
+
+
+@router.message(CommandStart(), F.from_user.id.in_(users))
+async def get_start(message: Message, bot):
+    await message.answer(f'Привет {message.from_user.first_name}. Начнем!')
+    chat_id = message.chat.id
+    await send_periodic_messages(bot, chat_id)
+
+
+@router.message(Command("today"), F.from_user.id.in_(users))
+async def get_start(message: Message):
+    quantity = get_quantity_phrases_repeat_today()
+    await message.answer(f'ок. сегодня повторяем фраз: {quantity}')
+
+
+async def send_periodic_messages(bot, chat_id):
+    list_message = get_list_messages_for_today()
+    logger.debug(f'Длина списка фраз на сегодня: {len(list_message)}')
+    while True:
+        current_time = datetime.datetime.now().time()
+        start_time = datetime.time(hour=9, minute=0)
+        end_time = datetime.time(hour=20, minute=0)
+        if start_time <= current_time <= end_time:
+            for message in list_message:
+                msg_rus = await bot.send_message(chat_id, message['russian'])
+                logger.debug(f'send message: {msg_rus}')
+                time.sleep(60)
+                msg_eng = await bot.send_message(chat_id, message['english'])
+                logger.debug(f'send message: {msg_eng}')
+                time.sleep(900)
+                await bot.delete_message(chat_id=chat_id, message_id=msg_rus.message_id)
+                logger.debug(f'del message: {msg_rus}')
+                await bot.delete_message(chat_id=chat_id, message_id=msg_eng.message_id)
+                logger.debug(f'del message: {msg_eng}')
+
+
+if __name__ == "__main__":
+    pass
---
+++ logic/prepare_info.py
@@ -0,0 +1,44 @@
+from typing import List, Union
+from database_functions.sql_select import get_phrases_x_days_ago
+from settings import get_logger
+
+logger = get_logger(__name__)
+
+
+def get_list_messages_for_today() -> List[dict]:
+    intervals = [1, 3, 7, 12, 16, 35, 50]
+    list_phrases = []
+    for x in intervals:
+        list_tuple = get_phrases_x_days_ago(x)
+        list_dict = [tuple_to_dict(x) for x in list_tuple]
+        list_phrases.extend(list_dict)
+    return list_phrases
+
+
+def get_list_phrases(list_tuple: Union[List[tuple], None]) -> List[str]:
+    list_phrases = []
+    if list_tuple is None:
+        return list_phrases
+    for x in list_tuple:
+        list_phrases.append(x[0])
+
+    return list_phrases
+
+
+def tuple_to_dict(input: tuple) -> dict:
+    phrases_dict = {'english': input[1], 'russian': input[0]}
+    return phrases_dict
+
+
+def get_quantity_phrases_repeat_today() -> int:
+    quantity = 0
+    try:
+        quantity = len(get_list_messages_for_today())
+    except Exception as error:
+        logger.error(error)
+
+    return quantity
+
+
+if __name__ == "__main__":
+    pass
